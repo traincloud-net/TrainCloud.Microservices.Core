621196adaa5dd67ebc708695ce6db733ace3bf90
Sebastian Hoyer <65442474+SebHoyer@users.noreply.github.com>
2023-11-16 23:22:25 +0100

    DataService

---
+++ TrainCloud.Microservices.Core/Services/DataService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace TrainCloud.Microservices.Core.Services;
+
+public abstract class DataService<TService, TDbContext> : AbstractService<TService>
+{
+    protected TDbContext DbContext { get; init; }
+
+    protected IDistributedCache Cache { get; init; }
+
+    protected DataService(IConfiguration configuration,
+                          ILogger<TService> logger,
+                          TDbContext dbContext,
+                          IDistributedCache cache)
+                          : base(configuration, logger)
+    {
+        DbContext = dbContext;
+        Cache = cache;
+    }
+}
