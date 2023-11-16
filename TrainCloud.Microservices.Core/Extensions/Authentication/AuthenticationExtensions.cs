9b0cfe10da8e51bcbc6298c87e72a8ef7349845c
Sebastian Hoyer <65442474+SebHoyer@users.noreply.github.com>
2023-11-16 09:20:55 +0100

    Extensions

---
+++ TrainCloud.Microservices.Core/Extensions/Authentication/AuthenticationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainCloud.Microservices.Core.Extensions.Authentication;
+
+public static class AuthenticationExtensions
+{
+    public static IServiceCollection AddTrainCloudAuthentication(this IServiceCollection services)
+    {
+
+        return services;
+    }
+}
---
+++ TrainCloud.Microservices.Core/Extensions/Swagger/SwaggerExtensions.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Reflection;
+
+namespace TrainCloud.Microservices.Core.Extensions.Swagger;
+
+public static class SwaggerExtensions
+{
+    public static IServiceCollection AddTrainCloudSwagger(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(swaggerGenOptions =>
+        {
+            swaggerGenOptions.EnableAnnotations();
+            swaggerGenOptions.ExampleFilters();
+
+            List<string> xmlFiles = Directory.GetFiles(AppContext.BaseDirectory, "*.xml", SearchOption.TopDirectoryOnly).ToList();
+            xmlFiles.ForEach(xmlFile => swaggerGenOptions.IncludeXmlComments(xmlFile, true));
+
+            swaggerGenOptions.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Title",// webApplicationBuilder.Configuration.GetSection("Swagger").GetValue<string>("Title"),
+                Description = "Description",// webApplicationBuilder.Configuration.GetSection("Swagger").GetValue<string>("Description"),
+                TermsOfService = new Uri("https://traincloud.net/Terms"),
+                Contact = new OpenApiContact
+                {
+                    Name = "TrainCloud",
+                    Email = "mail@traincloud.net"
+                },
+                License = new OpenApiLicense
+                {
+                    Name = "MIT License",
+                    Url = new Uri("https://TrainCloud/LICENSE.txt")
+                },
+                Version = "v1"
+            });
+
+            //swaggerGenOptions.DescribeAllEnumsAsStrings();
+
+            swaggerGenOptions.OperationFilter<SecurityRequirementsOperationFilter>(true, "Bearer");
+            swaggerGenOptions.OperationFilter<AppendAuthorizeToSummaryOperationFilter>();
+
+            // Include 'SecurityScheme' to use JWT Authentication
+            var jwtSecurityScheme = new OpenApiSecurityScheme
+            {
+                BearerFormat = "JWT",
+                Name = "JWT Authentication",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                Scheme = JwtBearerDefaults.AuthenticationScheme,
+                Description = "Put **ONLY** your JWT Bearer token in the textbox below!",
+                Reference = new OpenApiReference
+                {
+                    Id = JwtBearerDefaults.AuthenticationScheme,
+                    Type = ReferenceType.SecurityScheme
+                }
+            };
+            swaggerGenOptions.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+
+        });
+        services.AddSwaggerExamplesFromAssemblies(Assembly.GetEntryAssembly());
+        return services;
+    }
+
+    public static IApplicationBuilder UseTrainCloudSwagger(this IApplicationBuilder applicationBuilder)
+    {
+        applicationBuilder.UseSwagger();
+        applicationBuilder.UseSwaggerUI();
+        return applicationBuilder;
+    }
+
+}
