83f2a26aee062a0723ab8cd55033a4c2fa1e338a
Sebastian Hoyer <65442474+SebHoyer@users.noreply.github.com>
2023-11-16 18:49:30 +0100

    Auth

--- TrainCloud.Microservices.Core/Extensions/Authentication/AuthenticationExtensions.cs
+++ TrainCloud.Microservices.Core/Extensions/Authentication/AuthenticationExtensions.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,9 +15,27 @@ namespace TrainCloud.Microservices.Core.Extensions.Authentication;
 
 public static class AuthenticationExtensions
 {
-    public static IServiceCollection AddTrainCloudAuthentication(this IServiceCollection services)
+    public static IServiceCollection AddTrainCloudAuthentication(this IServiceCollection services, IConfiguration configuration)
     {
-
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddCookie(IdentityConstants.ApplicationScheme)
+        .AddJwtBearer(jwtBearerOptions =>
+                           {
+                               string jwtKey = configuration.GetSection("Identity").GetSection("Token").GetValue<string>("JwtSecurityKey")!;
+                               if (jwtKey is not null)
+                               {
+                                   jwtBearerOptions.TokenValidationParameters = new TokenValidationParameters()
+                                   {
+                                       ValidateIssuer = false,
+                                       ValidateAudience = false,
+                                       ValidateLifetime = true,
+                                       ValidateIssuerSigningKey = false,
+                                       ValidIssuer = configuration.GetSection("Identity").GetSection("Token").GetValue<string>("JwtIssuer"),
+                                       ValidAudience = configuration.GetSection("Identity").GetSection("Token").GetValue<string>("JwtAudience"),
+                                       IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+                                   };
+                               }
+                           });
         return services;
     }
 }
