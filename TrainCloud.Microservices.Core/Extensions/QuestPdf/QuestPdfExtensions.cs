c958f23ea357f5e14d861522434305035e91ae18
Sebastian Hoyer <65442474+SebHoyer@users.noreply.github.com>
2024-04-16 10:08:14 +0200

    Pdf

--- TrainCloud.Microservices.Core/Extensions/QuestPdf/QuestPdfExtensions.cs
+++ TrainCloud.Microservices.Core/Extensions/QuestPdf/QuestPdfExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using QuestPDF.Drawing;
 using QuestPDF.Infrastructure;
 
 namespace TrainCloud.Microservices.Core.Extensions.QuestPdf;
@@ -12,39 +11,6 @@ public static class QuestPdfExtensions
         QuestPDF.Settings.CheckIfAllTextGlyphsAreAvailable = false;
         QuestPDF.Settings.EnableDebugging = true;
         QuestPDF.Settings.UseEnvironmentFonts = false;
-
-        var rootFolder = Directory.GetCurrentDirectory();
-        var fontFolder = Path.Combine(rootFolder, "wwwroot", "fonts");
-
-        var fontFile = Path.Combine(fontFolder, "Lato-Black.ttf");
-        FontManager.RegisterFont(File.OpenRead(fontFile));
-
-        fontFile = Path.Combine(fontFolder, "Lato-BlackItalic.ttf");
-        FontManager.RegisterFont(File.OpenRead(fontFile));
-
-        fontFile = Path.Combine(fontFolder, "Lato-Bold.ttf");
-        FontManager.RegisterFont(File.OpenRead(fontFile));
-
-        fontFile = Path.Combine(fontFolder, "Lato-BoldItalic.ttf");
-        FontManager.RegisterFont(File.OpenRead(fontFile));
-
-        fontFile = Path.Combine(fontFolder, "Lato-Italic.ttf");
-        FontManager.RegisterFont(File.OpenRead(fontFile));
-
-        fontFile = Path.Combine(fontFolder, "Lato-Light.ttf");
-        FontManager.RegisterFont(File.OpenRead(fontFile));
-
-        fontFile = Path.Combine(fontFolder, "Lato-LightItalic.ttf");
-        FontManager.RegisterFont(File.OpenRead(fontFile));
-
-        fontFile = Path.Combine(fontFolder, "Lato-Regular.ttf");
-        FontManager.RegisterFont(File.OpenRead(fontFile));
-
-        fontFile = Path.Combine(fontFolder, "Lato-Thin.ttf");
-        FontManager.RegisterFont(File.OpenRead(fontFile));
-
-        fontFile = Path.Combine(fontFolder, "Lato-ThinItalic.ttf");
-        FontManager.RegisterFont(File.OpenRead(fontFile));
 
         return services;
     }
