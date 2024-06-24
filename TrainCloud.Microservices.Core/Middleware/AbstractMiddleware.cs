4dec9be218fb38b8c5cb7e98d5b14495510a9a1f
Sebastian Hoyer <65442474+SebHoyer@users.noreply.github.com>
2024-06-24 21:42:26 +0200

    LocalizationMiddleware

---
+++ TrainCloud.Microservices.Core/Middleware/AbstractMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace TrainCloud.Microservices.Core.Middleware;
+
+public abstract class AbstractMiddleware<TMiddleware>
+{
+    protected RequestDelegate Next { get; init; }
+
+    protected IConfiguration Configuration { get; init; }
+
+    protected ILogger<TMiddleware> Logger { get; init; }
+
+    public AbstractMiddleware(RequestDelegate next,
+                              IConfiguration configuration,
+                              ILogger<TMiddleware> logger)
+    {
+        Next = next;
+        Configuration = configuration;
+        Logger = logger;
+    }
+}
---
+++ TrainCloud.Microservices.Core/Middleware/Localization/LocalizationMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Globalization;
+
+namespace TrainCloud.Microservices.Core.Middleware.Localization;
+
+public class LocalizationMiddleware : AbstractMiddleware<LocalizationMiddleware>
+{
+    public LocalizationMiddleware(RequestDelegate next,
+                                  IConfiguration configuration,
+                                  ILogger<LocalizationMiddleware> logger)
+        : base(next, configuration, logger)
+    {
+
+    }
+
+    public Task Invoke(HttpContext httpContext)
+    {
+        string? trainCloudHeader = httpContext.Request.Headers["TrainCloud-Header"];
+        if (!string.IsNullOrEmpty(trainCloudHeader) && trainCloudHeader == "TrainCloud")
+        {
+            string? acceptLanguage = httpContext.Request.Headers["Accept-Language"];
+
+
+            string requestCulture = "en";
+            if (!string.IsNullOrEmpty(acceptLanguage))
+            {
+                requestCulture = acceptLanguage.Substring(0, 2);
+            }
+
+            CultureInfo culture = new(requestCulture);
+
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+        }
+
+        return Next(httpContext);
+    }
+}
