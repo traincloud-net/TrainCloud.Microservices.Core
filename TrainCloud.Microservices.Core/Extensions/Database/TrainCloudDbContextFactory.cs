364588c27e2534caeab4b04313ae7fe5a210cafe
Sebastian Hoyer <65442474+SebHoyer@users.noreply.github.com>
2024-10-16 20:34:26 +0200

    TrainCloudDbContextFactory

---
+++ TrainCloud.Microservices.Core/Extensions/Database/TrainCloudDbContextFactory.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace TrainCloud.Microservices.Core.Extensions.Database;
+
+/// <summary>
+/// 
+/// </summary>
+/// <typeparam name="TGenericDbContext"></typeparam>
+/// <typeparam name="TPostgresDbContext"></typeparam>
+/// <typeparam name="TSqliteDbContext"></typeparam>
+public sealed class TrainCloudDbContextFactory<TGenericDbContext, TPostgresDbContext, TSqliteDbContext> : IDbContextFactory<TGenericDbContext>
+    where TGenericDbContext : DbContext
+    where TPostgresDbContext : TGenericDbContext
+    where TSqliteDbContext : TGenericDbContext
+{
+    private IConfiguration Configuration { get; init; }
+
+    public TrainCloudDbContextFactory(DbContextOptions options, IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public TGenericDbContext CreateDbContext()
+    {
+        string databaseProvider = Configuration.GetValue<string>("Database:Provider")!;
+        TGenericDbContext? dbContext = null;
+        switch (databaseProvider.ToLower())
+        {
+            case "postgresql":
+                dbContext = (TPostgresDbContext)Activator.CreateInstance(typeof(TPostgresDbContext), Configuration)!;
+                break;
+            case "sqlite":
+                dbContext = (TSqliteDbContext)Activator.CreateInstance(typeof(TSqliteDbContext), Configuration)!;
+                break;
+        }
+
+        return dbContext!;
+    }
+}
