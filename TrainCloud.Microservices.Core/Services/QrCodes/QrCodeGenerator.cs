1484ea0b1ba8ca7cf8e8f2a357061a41f86ea860
Sebastian Hoyer <65442474+SebHoyer@users.noreply.github.com>
2025-09-29 10:53:46 +0200

    d

---
+++ TrainCloud.Microservices.Core/Services/QrCodes/QrCodeGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainCloud.Microservices.Core.Services.QrCodes;
+
+public sealed class QrCodeGenerator : IQrCodeGenerator
+{
+}
---
+++ TrainCloud.Microservices.Core/Services/QrCodes/QrCodeGeneratorExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TrainCloud.Microservices.Core.Services.QrCodes;
+
+public static class QrCodeGeneratorExtensions
+{
+    /// <summary>
+    /// Adds the QrCodeGenerator to the Microservice DI container.
+    /// Singleton
+    /// https://cloud.google.com/dotnet/docs/reference/Google.Cloud.PubSub.V1/latest#performance-considerations-and-default-settings
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddTrainCloudQrCodeGenerator(this IServiceCollection services)
+    {
+        services.AddScoped<IQrCodeGenerator, QrCodeGenerator>();
+
+        return services;
+    }
+}
