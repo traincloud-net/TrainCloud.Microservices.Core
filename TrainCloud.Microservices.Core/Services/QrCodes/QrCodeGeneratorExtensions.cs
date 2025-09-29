f84d303a53659a58375328aca461021f09a2dbb9
Sebastian Hoyer <65442474+SebHoyer@users.noreply.github.com>
2025-09-29 12:54:14 +0200

    f

--- TrainCloud.Microservices.Core/Services/QrCodes/QrCodeGeneratorExtensions.cs
+++ TrainCloud.Microservices.Core/Services/QrCodes/QrCodeGeneratorExtensions.cs
@@ -14,7 +14,6 @@ public static class QrCodeGeneratorExtensions
     public static IServiceCollection AddTrainCloudQrCodeGenerator(this IServiceCollection services)
     {
         services.AddScoped<IQrCodeGenerator, QrCodeGenerator>();
-
         return services;
     }
 }
