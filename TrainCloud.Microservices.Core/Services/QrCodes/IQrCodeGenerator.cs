46eb337bc7d2bc43217031d9ba714f396f68ca8c
Sebastian Hoyer <65442474+SebHoyer@users.noreply.github.com>
2025-09-29 11:18:38 +0200

    QrCode

--- TrainCloud.Microservices.Core/Services/QrCodes/IQrCodeGenerator.cs
+++ TrainCloud.Microservices.Core/Services/QrCodes/IQrCodeGenerator.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace TrainCloud.Microservices.Core.Services.QrCodes;
 
-namespace TrainCloud.Microservices.Core.Services.QrCodes;
 internal interface IQrCodeGenerator
 {
+    byte[] GenerateQrCodePng(string codeData);
 }
--- TrainCloud.Microservices.Core/Services/QrCodes/QrCodeGenerator.cs
+++ TrainCloud.Microservices.Core/Services/QrCodes/QrCodeGenerator.cs
@@ -1,11 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using QRCoder;
 
 namespace TrainCloud.Microservices.Core.Services.QrCodes;
 
 public sealed class QrCodeGenerator : IQrCodeGenerator
 {
+    public System.Drawing.Color DarkColor { get; } = System.Drawing.Color.Black;
+
+    public System.Drawing.Color LightColor { get; } = System.Drawing.Color.White;
+
+    public int PixelsPerModule { get; } = 5;
+
+    public bool DrawQuietZones { get; } = true;
+
+    public byte[] GenerateQrCodePng(string codeData)
+    {
+        QRCodeGenerator.ECCLevel eccLevel = QRCodeGenerator.ECCLevel.H;
+
+        using QRCodeGenerator qrGenerator = new();
+        using QRCodeData qrCodeData = qrGenerator.CreateQrCode(codeData, eccLevel);
+        using PngByteQRCode qrCode = new(qrCodeData);
+
+        byte[] imageBytes = qrCode.GetGraphic(PixelsPerModule, DarkColor, LightColor, DrawQuietZones);
+
+        return imageBytes;
+    }
 }
